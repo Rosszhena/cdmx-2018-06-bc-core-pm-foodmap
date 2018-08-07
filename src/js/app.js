b4477b03810f659f38bf23677eda2371dfbd227f
Maria Rosa Rosete <ross.zhena@gmail.com>
2018-08-07 12:54:55 -0500

    Se agrega diseño a la vista principal

--- src/js/app.js
+++ src/js/app.js
@@ -1,64 +1,74 @@
 var map;
-      function initMap() {
-        // Create the map.
-        var pyrmont = {lat: 19.4978, lng: -99.1269};
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: pyrmont,
-          zoom: 17
-        });
+var infowindow;
 
-        // Create the places service.
-        var service = new google.maps.places.PlacesService(map);
-        var getNextPage = null;
-        var moreButton = document.getElementById('more');
-        moreButton.onclick = function() {
-          moreButton.disabled = true;
-          if (getNextPage) getNextPage();
-        };
+var request;
+var service;
+var markers = [];
 
-        // Perform a nearby search.
-        service.nearbySearch(
-            {location: pyrmont, radius: 500, type: ['restaurant']},
-            function(results, status, pagination) {
-              if (status !== 'OK') return;
-
-              createMarkers(results);
-              moreButton.disabled = !pagination.hasNextPage;
-              getNextPage = pagination.hasNextPage && function() {
-                pagination.nextPage();
-              };
-            });
-      }
-
-      function createMarkers(places) {
-        var bounds = new google.maps.LatLngBounds();
-        var placesList = document.getElementById('places');
-        //console.log(placesList)
-
-        for (var i = 0, place; place = places[i]; i++) {
-            
-          var image = {
-            url: place.icon,
-            size: new google.maps.Size(71, 71),
-            origin: new google.maps.Point(0, 0),
-            anchor: new google.maps.Point(17, 34),
-            scaledSize: new google.maps.Size(25, 25)
-          };
-
-          var marker = new google.maps.Marker({
-            map: map,
-            icon: image,
-            title: place.name,
-            position: place.geometry.location
-          });
-
-          var li = document.createElement('li');
-          li.textContent = place.name;
-          placesList.appendChild(li);
-
-          bounds.extend(place.geometry.location);
-          console.log(place.geometry.location)
-        }
-        map.fitBounds(bounds);
-      }
+function initialize(){
+    var center = new google.maps.LatLng(19.4978, -99.1269);
+    map = new google.maps.Map(document.getElementById("map"),{
+        center:center,
+        zoom:13
+    });
     
+    var request = {
+        location: center,
+        radius: 8047,
+        types: ['Restaurante']
+    };
+    
+    infowindow = new google.maps.InfoWindow();
+    var service = new google.maps.places.PlacesService(map);
+   
+    service.nearbySearch(request, callback);
+
+
+    google.maps.event.addListener(map, 'rightclick',function(event){
+        map.setCenter(event.LatLng)
+        clearResults(markers)
+
+        var request = {
+            location: event.LatLng,
+            radius: 8047,
+            types: ['restaurant']
+        };
+        service.nearbySearch(request, callback);
+    })
+}
+
+function callback(results, status){
+    if(status == google.maps.places.PlacesServiceStatus.OK){
+        for(var i = 0; i < results.length; i++){
+            markers.push(createMarker(results[i]));
+        }
+    }
+}
+
+function createMarker(place){
+    var placeLoc = place.geometry.location;
+    var marker = new google.maps.Marker({
+        map: map,
+        position: place.geometry.location
+        
+         });
+
+         google.maps.event.addListener(marker, 'click',function(){
+             console.log(place.name)
+             console.log(place.vicinity)
+             infowindow.setContent(place.name + " " +  place.vicinity);
+             infowindow.open(map, this);
+             maxWidth: 200
+         });
+
+         return marker;
+     }      
+     
+     function clearResults(markers){
+         for(var m in markers){
+             markers[m].setMap(null)
+         }
+         markers = []
+     }
+
+google.maps.event.addDomListener(window, 'load', initialize);
