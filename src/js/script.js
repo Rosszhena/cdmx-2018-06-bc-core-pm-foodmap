669db22cd4ec1286d00baf0a24fa6297c6b6814b
Maria Rosa Rosete <ross.zhena@gmail.com>
2018-08-07 10:04:59 -0500

    Se estructura codigo y se une con ventana splash

--- src/js/script.js
+++ src/js/script.js
@@ -6,7 +6,7 @@
     function frame() {
         if(loading == 100) {
             clearInterval(id);
-            window.open("welcome.html", "_self");
+            window.open("foodmap.html", "_self");
         }else{
             loading = loading + 1;
             if(loading == 90){
